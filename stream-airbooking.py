e776b1f1ebd5a85c14c917c6936a6cc308b82f46
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-06 11:38:42 +0700

    stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -1,35 +1,46 @@
 import pickle
-import streamlit as st
+import pandas as pd  # type: ignore
+import streamlit as st  # type: ignore
 
-airbooking_model = pickle.load(open('customer_booking.csv', 'rb'))
+# Membaca data dari file CSV
+airbooking_data = pd.read_csv('customer_booking.csv')
 
-st.title('Prediksi Airbooking anlysis')
+# Menampilkan beberapa baris pertama dari data
+print(airbooking_data.head())
 
-col1, col2 = st.columnns(2)
+# Memuat model prediksi yang disimpan dalam file pickle
+with open('model.pkl', 'rb') as model_file:
+    airbooking_model = pickle.load(model_file)
 
-with col1 :
-    sales_channel = st.text_input ('Input Sales Channel')
+st.title('Prediksi Airbooking Analysis')
 
-with col2 :
-    Trip_type = st.text_input ('Input Trip Type')
+# Membuat kolom input
+col1, col2 = st.columns(2)
 
-with col1 :
-    flight_day = st.text_input ('Input Flight Day')
+with col1:
+    sales_channel = st.text_input('Input Sales Channel')
 
-with col2 :
-    route = st.text_input ('Input Route')
+with col2:
+    trip_type = st.text_input('Input Trip Type')
 
-with col1 :
-    booking_origin = st.text_input ('Input Booking Origin')
+with col1:
+    flight_day = st.text_input('Input Flight Day')
 
-airbook_predict = ''
+with col2:
+    route = st.text_input('Input Route')
 
-if st.button('Tes prediksi '):
-        airbook_prediction = airbooking_model.predict([[sales_channel,Trip_type,flight_day,route,booking_origin]])
+with col1:
+    booking_origin = st.text_input('Input Booking Origin')
 
-        if(airbook_prediction[0] == 1):
-             airbook_prediction = 'Perjalanan anda tepat'
-        else :
-             airbook_prediction = 'Perjalanan anda kurang tepat'
+airbook_prediction = ''
 
-st.success(airbook_predict)
+if st.button('Tes Prediksi'):
+    # Melakukan prediksi dengan model
+    prediction = airbooking_model.predict([[sales_channel, trip_type, flight_day, route, booking_origin]])
+
+    if prediction[0] == 1:
+        airbook_prediction = 'Perjalanan anda tepat'
+    else:
+        airbook_prediction = 'Perjalanan anda kurang tepat'
+
+st.success(airbook_prediction)
