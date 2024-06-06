026133536f50cebe53c5fc9e3b1ba7fd7f0eaea2
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-06 14:48:45 +0700

    stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -1,49 +1,12 @@
-import os
-import pandas as pd  
-import streamlit as st  
 import logging
+import pickle
+import pandas as pd  
+import streamlit as st 
+import os
 
+airbook_model = pd.read_cvs('customer_booking')
 
-logging.basicConfig(level=logging.INFO)
-
-# Jalur file
-csv_path = 'customer_booking.csv'
-model_path = 'airlines_booking_uas'
-
-# Memastikan file CSV dan model ada
-airbooking_data = None
-airbooking_model = None
-
-# Memeriksa keberadaan file model
-if not os.path.exists(model_path):
-    logging.error(f"File model tidak ditemukan: {model_path}")
-    st.error(f"File model tidak ditemukan: {model_path}")
-else:
-    # Memuat model prediksi yang disimpan dalam file pickle
-    try:
-        with open(model_path, 'rb') as model_file:
-            airbooking_model = pickle.load(model_file)
-            logging.info(f"Model dimuat dengan sukses dari {model_path}.")
-    except Exception as e:
-        logging.error(f"Terjadi kesalahan saat memuat model: {e}")
-        st.error(f"Terjadi kesalahan saat memuat model: {e}")
-
-
-# Memeriksa keberadaan file model
-if not os.path.exists(model_path):
-    logging.error(f"File model tidak ditemukan: {model_path}")
-    st.error(f"File model tidak ditemukan: {model_path}")
-else:
-    # Memuat model prediksi yang disimpan dalam file pickle
-    try:
-        with open(model_path, 'rb') as model_file:
-            airbooking_model = pickle.load(model_file)
-            logging.info(f"Model dimuat dengan sukses dari {model_path}.")
-    except Exception as e:
-        logging.error(f"Terjadi kesalahan saat memuat model: {e}")
-        st.error(f"Terjadi kesalahan saat memuat model: {e}")
-
-st.title('Prediksi Airbooking Analysis')
+st.title('Prediksi Model Airbooking')
 
 # Membuat kolom input
 col1, col2 = st.columns(2)
@@ -68,12 +31,12 @@ airbook_prediction = ''
 if st.button('Tes Prediksi'):
     if any(not val for val in [sales_channel, trip_type, flight_day, route, booking_origin]):
         st.error("Semua input harus diisi.")
-    elif airbooking_model is None:
+    elif airbook_model is None:
         st.error("Model prediksi tidak tersedia.")
     else:
         try:
             # Melakukan prediksi dengan model
-            prediction = airbooking_model.predict([[sales_channel, trip_type, flight_day, route, booking_origin]])
+            prediction = airbook_model.predict([[sales_channel, trip_type, flight_day, route, booking_origin]])
             logging.info("Prediksi berhasil dilakukan.")
 
             if prediction[0] == 1:
