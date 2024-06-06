8cd6378906fc8ca4435162500b0af7ad3b32ace1
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-06 12:03:23 +0700

    stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -1,33 +1,45 @@
+import os
 import pickle
 import pandas as pd  # type: ignore
 import streamlit as st  # type: ignore
-import os
-
-# Menambahkan logging
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
-# Memastikan file CSV dan model ada
+# Jalur file
 csv_path = 'customer_booking.csv'
 model_path = 'model.pkl'
 
+# Memastikan file CSV dan model ada
 if not os.path.exists(csv_path):
     logging.error(f"File CSV tidak ditemukan: {csv_path}")
     st.error(f"File CSV tidak ditemukan: {csv_path}")
 else:
-    # Membaca data dari file CSV
-    airbooking_data = pd.read_csv(csv_path)
-    logging.info("File CSV dibaca dengan sukses.")
-    print(airbooking_data.head())
+    # Membaca data dari file CSV dengan encoding yang berbeda jika utf-8 gagal
+    try:
+        airbooking_data = pd.read_csv(csv_path, encoding='utf-8')
+        logging.info("File CSV dibaca dengan sukses dengan encoding utf-8.")
+    except UnicodeDecodeError as e:
+        logging.warning(f"Kesalahan decoding utf-8: {e}")
+        try:
+            airbooking_data = pd.read_csv(csv_path, encoding='latin1')
+            logging.info("File CSV dibaca dengan sukses dengan encoding latin1.")
+        except Exception as e:
+            logging.error(f"Terjadi kesalahan saat membaca file CSV: {e}")
+            st.error(f"Terjadi kesalahan saat membaca file CSV: {e}")
 
 if not os.path.exists(model_path):
     logging.error(f"File model tidak ditemukan: {model_path}")
     st.error(f"File model tidak ditemukan: {model_path}")
 else:
     # Memuat model prediksi yang disimpan dalam file pickle
-    with open(model_path, 'rb') as model_file:
-        airbooking_model = pickle.load(model_file)
-        logging.info("Model dimuat dengan sukses.")
+    try:
+        with open(model_path, 'rb') as model_file:
+            airbooking_model = pickle.load(model_file)
+            logging.info("Model dimuat dengan sukses.")
+    except Exception as e:
+        logging.error(f"Terjadi kesalahan saat memuat model: {e}")
+        st.error(f"Terjadi kesalahan saat memuat model: {e}")
 
 st.title('Prediksi Airbooking Analysis')
 
