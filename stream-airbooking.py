a887e09e91a0b4de2ccc7d17a9b618482c73d55b
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-06 14:06:40 +0700

    stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Jalur file
 csv_path = 'customer_booking.csv'
-model_path = 'airlines_booking_uas.model'
+model_path = 'airlines_booking_uas.py'
 
 # Memastikan file CSV dan model ada
 airbooking_data = None
