5022f7bd480a06560795ed2f0170e71e031902e6
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-07 16:47:13 +0700

    stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -115,4 +115,4 @@ if st.button('Tes Prediksi'):
             logging.error(f"Terjadi kesalahan saat prediksi: {e}")
             st.error(f"Terjadi kesalahan saat prediksi: {e}")
 
-st.success(airbook_prediction
+st.success(airbook_prediction)
