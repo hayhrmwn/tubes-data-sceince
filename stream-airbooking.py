4d18eaa7bfc42dfa300e43b2a778c4093b018de3
hayhrmwn <125879476+hayhrmwn@users.noreply.github.com>
2024-06-08 10:35:08 +0700

    Update stream-airbooking.py

--- stream-airbooking.py
+++ stream-airbooking.py
@@ -98,4 +98,14 @@ except Exception as e:
 wants_baggage = "wants extra baggage" if prediction > 0.5 else "doesn't want extra baggage"
 
 # Display prediction result using st.success
-st.success(f"Prediction: {prediction:.2f}\nThe customer {w
+st.success(f"Prediction: {prediction:.2f}\nThe customer {wants_baggage}")
+
+# Save the model locally
+try:
+    joblib.dump(rf_model, local_model_path, compress=True)
+except Exception as e:
+    st.error(f"Error saving the model: {e}")
+
+if __name__ == '__main__':
+    st._is_running_with_streamlit = True
+    st.run()
